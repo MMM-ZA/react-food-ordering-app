35e84a01f9b7f384064ac38f06957f76d29b4bd2
makoma mabilo <makoma44@gmail.com>
2023-02-02 14:02:59 +0800

    adding individual meal items and displaying them

--- react-food-ordering-app/src/Components/Meals/AvailableMeals.js
+++ react-food-ordering-app/src/Components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem';
+import Card from '../UI/Card';
 
 const DUMMY_MEALS = [
   {
@@ -29,11 +31,18 @@ const DUMMY_MEALS = [
 ]
 
 const AvailableMeals = () => {
- const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+ const mealsList = DUMMY_MEALS.map((meal) => <MealItem
+  key={meal.id}
+  name={meal.name}
+  description={meal.description}
+  price={meal.price}
+   /> );
 
  return (
 <section className={classes.meals}>
+  <Card>
    <ul>{mealsList}</ul>
+   </Card>
 </section>
  );
 };
