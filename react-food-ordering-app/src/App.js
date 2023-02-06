ce5806ea40225259e8339048b872d7785899f61e
makoma mabilo <makoma44@gmail.com>
2023-02-06 13:36:01 +0800

    added a Modal via React Portal

--- react-food-ordering-app/src/App.js
+++ react-food-ordering-app/src/App.js
@@ -2,10 +2,12 @@ import { Fragment } from 'react';
 import './App.css';
 import Header from './Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
+import Cart from './Components/Cart/Cart';
 
 function App() {
   return (
     <Fragment>
+    <Cart/>
     <Header/>
     <main>
       <Meals/>
