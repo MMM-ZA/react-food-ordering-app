b12a0531132febc286c04fb8a065d1e97ef3012c
makoma mabilo <makoma44@gmail.com>
2023-02-20 14:19:43 +0800

    working with refs, forwarding refs

--- react-food-ordering-app/src/Components/Meals/AvailableMeals.js
+++ react-food-ordering-app/src/Components/Meals/AvailableMeals.js
@@ -51,6 +51,7 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
  const mealsList = DUMMY_MEALS.map((meal) => <MealItem
   key={meal.id}
+  id={meal.id}
   name={meal.name}
   description={meal.description}
   price={meal.price}
