46fe26c3601bb2487d24878d5b5887dfc617bcf4
makoma mabilo <makoma44@gmail.com>
2023-02-27 13:35:31 +0800

    outputting cartItems

--- react-food-ordering-app/src/Components/Cart/Cart.js
+++ react-food-ordering-app/src/Components/Cart/Cart.js
@@ -2,16 +2,26 @@ import { useContext } from 'react';
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
  const cartCtx = useContext(CartContext)
+
+ const cartItemAddHandler = (item) => {};
+
+ const cartItemRemoveHandler = (id) => {};
 
    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
   const cartItems = (
     <ul className={classes['cart-items']}>
      {cartCtx.map((item) => (
-        <li>{item.name}</li>
+      <CartItem key={item.id}
+       name={item.name}
+        amount={item.amount}
+         price={item.price}
+         onRemove={cartItemRemoveHandler.bind(null,item)}
+         onAdd={cartItemAddHandler.bind(null,item)}/>
       ))}
     </ul>
   );
--- react-food-ordering-app/src/Components/Layout/HeaderCartButton.js
+++ react-food-ordering-app/src/Components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext } from 'react';
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
