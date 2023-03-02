94b397973162f543bfa16a8e08a480897c5b2657
makoma mabilo <makoma44@gmail.com>
2023-03-02 21:01:48 +0800

    completed and fixed HeaderCartButton

--- react-food-ordering-app/src/Components/Layout/HeaderCartButton.js
+++ react-food-ordering-app/src/Components/Layout/HeaderCartButton.js
@@ -1,17 +1,40 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
   const cartCtx = useContext(CartContext);
+  const [btnIsHighlighted,setBtnIsHighlighted] = useState(false);
+
+
+  const {items} = cartCtx;
 
   const numberOfCartItems =cartCtx.items.reduce((curNumber,item) => {
    return curNumber + item.amount
   },0);
 
+    const btnClasses = `${classes.button}  ${ btnIsHighlighted ?  classes.bump: '' }`;
+
+    useEffect(() => {
+
+     if (items.length === 0 ) {
+      return;
+     }
+     setBtnIsHighlighted(true);
+
+   const timer = setTimeout(() => {
+    setBtnIsHighlighted(false);
+
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+   }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
     <span className={classes.icon}>
     <CartIcon/>
     </span>
--- react-food-ordering-app/src/Components/UI/Modal.js
+++ react-food-ordering-app/src/Components/UI/Modal.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import classes from './Modal.module.css';
 
+
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={props.onClose} />;
 };
